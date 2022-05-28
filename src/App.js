d8ffcae7ffb9389077f8486935a750df1df9df89
Nick <przebienda.n@gmail.com>
2022-05-28 15:59:50 -0400

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "tachyons";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="ba" style={{ borderWidth: 5 }}>
+      nothing
+    </div>
+  );
+}
+
+export default App;
